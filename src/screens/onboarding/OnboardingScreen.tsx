f4fcb3ba8a42b4d5a2dcc8ec17a28d23f28dab7f
christina8711 <christina.nguyen8711@gmail.com>
2023-03-19 19:56:32 -0400

    Developed parrallax image carousel W/ texts

--- src/screens/onboarding/OnboardingScreen.tsx
+++ src/screens/onboarding/OnboardingScreen.tsx
@@ -1,18 +1,26 @@
 import React from 'react'
-import { Image, Text, View, StyleSheet,StatusBar, Dimensions, Animated } from 'react-native'
+import { Image, Text, View, StyleSheet,StatusBar, Dimensions, Animated} from 'react-native'
+import { scale } from 'react-native-size-matters'
 const {width,height} = Dimensions.get('window')
+//../../../assets/AppLogo
 const items = [{
   id: 1,
-  image: require('./1.jpg'),
-  title: "Find the right workout for what you need"
+  image: require('../../../assets/1.jpg'),
+  indicator:require('../../../assets/Indicator.png'),
+  title: "Find the right\n workout for what you need",
+  description: "Fit 4 U is the ideal program for creating workouts tailored to your specific needs, whether you want to lose weight, gain strength, or simply keep active. Train up to seven days each week. "
 },{
   id: 2,
-  image: require('./2.jpg'),
-  title: "Unlimited support to stay on track" 
+  image: require('../../../assets/2.jpg'),
+  indicator:require('../../../assets/Indicator2.png'),
+  title: "Unlimited support to stay\n on track",
+  description:"Fit 4 U provides daily check-ins as well as a memory album of your progress to keep you motivated, guide your workout, and make any changes needed based on your work schedule or travel."
 },{
   id: 3,
-  image: require('./3.jpg'),
-  title: "Your own custom designed workout plan"
+  image: require('../../../assets/3.jpg'),
+  indicator:require('../../../assets/Indicator3.png'),
+  title: "Your own custom designed\n workout plan",
+  description:"Your designed workouts will be split into certain amount of days\n you plan to exercise per week based on your schedule\n and progress."
 }]
 
 const OnboardingScreen = () => {
@@ -48,10 +56,10 @@ const OnboardingScreen = () => {
               }]
             }
           ]} 
-          resizeMode="cover"
+          resizeMode='stretch'
         />
       <View style={styles.fixedImageContainer}>
-        <Image source={require('./TextBackground.png')} style={styles.fixedImage} />
+        <Image source={require('../../../assets/TextBackground.png')} style={styles.fixedImage} />
       </View>
         <Animated.View 
           style={[
@@ -69,16 +77,59 @@ const OnboardingScreen = () => {
               }]
               
             }
-          ]}
+          ,]}
         >
-        <Text style={styles.title}>{item.title}</Text>
+        <Text numberOfLines={2} ellipsizeMode='tail' style={styles.title}>{item.title}</Text>
         </Animated.View>
         <Animated.View style={StyleSheet.absoluteFillObject}/>
+        <Animated.View 
+          style={[
+            styles.descriptionContainer,
+            {
+              opacity: scrollAnimation.interpolate({
+                inputRange: inputRange,
+                outputRange: [0, 1, 0]
+              }),
+              transform:[{
+                translateX: scrollAnimation.interpolate({
+                  inputRange: inputRange,
+                  outputRange: [250, 0, -250],
+                })
+              }]
+              
+            }
+          ,]}
+        >
+        <Text numberOfLines={3} ellipsizeMode='tail' style={styles.description}>{item.description}</Text>
+        </Animated.View>
+        <Animated.View style={StyleSheet.absoluteFillObject}/>
+
+        <Animated.Image 
+          source={item.indicator} 
+          style={[
+            styles.indicatorContainer,
+            {
+              opacity: scrollAnimation.interpolate({
+                inputRange: inputRange,
+                outputRange: [0, 1, 0]
+              }),
+              transform:[{
+                translateX: scrollAnimation.interpolate({
+                  inputRange: inputRange,
+                  outputRange: [250, 0, -250],
+                })
+              }]
+            }
+          ]} 
+        />
         </View>
+        
       )
     }}
      />
-
+      <View style={styles.appTitleContainer}>
+        <Image source={require('../../../assets/Fit-4-U.png')} style={styles.appTitle} />
+      </View>
     </View>
     
   )
@@ -97,31 +148,69 @@ const styles = StyleSheet.create({
     height,
   },
   image:{
-    width,
-    height,
+    width: width < 420 ? 412:width,
+    height: height < 900 ? 740:height,
     zIndex: 0
-
-    
   },
   titleContainer:{
     position: 'absolute',
-    bottom: 80,
-    zIndex: 3
+    top: height < 900 ? (height * .695):(height * .69),
+    zIndex: 3,
   },
   title:{
-    fontSize: 24,
+    fontSize: height < 900 ? 25:30,
     color:'#ffffff',
+    textAlign: 'center',
+    lineHeight: height < 900 ? 30:45,
+    textShadowOffset: { width: 0, height: 4 },
+    textShadowRadius: 4,
+    textShadowColor: 'rgba(0, 0, 0, 0.25)'
+  },
+  descriptionContainer:{
+    position: 'absolute',
+    top: height * .79,
+    zIndex: 3,
+  },
+  description:{
+    fontSize: scale(11),
+    color:'#ffffff',
+    textAlign: 'center',
+
+    fontWeight: '400',
+    lineHeight: 18,
+  },
+  appTitleContainer: {
+    position: 'absolute',
+    top: height < 900 ? (height * 0.015):(height * 0.036),
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    zIndex: 10,
+  },
+  appTitle: {
+    width: (width*0.48),
+    height: (height*0.077),
+    resizeMode: 'contain',
   },
   fixedImageContainer:{
     position: 'absolute',
-    bottom: 0,
-    zIndex: 2
+    bottom: -10,
+    zIndex: 2,
   },
   fixedImage:{
     width,
-    height: 299,
+    height: (height * 0.32),
+    borderTopRightRadius: 48,
+    borderTopLeftRadius: 48,
     resizeMode: 'cover',
   },
-})
+  indicatorContainer:{
+    width: (width * .25),
+    height: 4,
+    bottom: height < 900 ? (height * 0.32 - 133):(height * 0.32 - 180),
+    zIndex: 3,
+    resizeMode: 'contain'
 
+  }
+})
 export default OnboardingScreen
