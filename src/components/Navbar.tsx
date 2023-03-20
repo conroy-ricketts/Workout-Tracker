e704e0c755ea3de830e5bc9f07f5175ac8cf729f
christina8711 <christina.nguyen8711@gmail.com>
2023-03-19 22:22:10 -0400

    added suggested changes!

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Image } from 'react-native';
+import { Image,StyleSheet,Dimensions} from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import TrackerScreen from '../screens/core/TrackerScreen';
 import SettingsScreen from '../screens/core/SettingsScreen';
 import StatsScreen from '../screens/core/StatsScreen';
 import { getTabBarHeight } from '@react-navigation/bottom-tabs/lib/typescript/src/views/BottomTabBar';
 
+const {width,height} = Dimensions.get('window')
 const Tab = createBottomTabNavigator();
 
 type TabBarIconProps = {
@@ -16,24 +17,24 @@ type TabBarIconProps = {
 const TrackerTabOptions = {
   tabBarIcon: ({ focused }: TabBarIconProps) => (
     focused ?
-      <Image source={require('../../assets/NavBarIcons/SelectedHome.png')} /> :
-      <Image source={require('../../assets/NavBarIcons/HomeTab.png')} />
+      <Image source={require('../../assets/NavBarIcons/SelectedHome.png')} style={styles.icon} /> :
+      <Image source={require('../../assets/NavBarIcons/HomeTab.png')} style={styles.icon} />
   )
 }
 
 const SettingsTabOptions = {
   tabBarIcon: ({ focused }: TabBarIconProps) => (
     focused ?
-      <Image source={require('../../assets/NavBarIcons/SelectedProfile.png')} /> :
-      <Image source={require('../../assets/NavBarIcons/ProfileIcon.png')} />
+      <Image source={require('../../assets/NavBarIcons/SelectedProfile.png')} style={styles.icon} /> :
+      <Image source={require('../../assets/NavBarIcons/ProfileIcon1.png')} style={styles.icon}/>
   )
 }
 
 const StatsTabOptions = {
   tabBarIcon: ({ focused }: TabBarIconProps) => (
     focused ?
-      <Image source={require('../../assets/NavBarIcons/SelectedStats.png')} /> :
-      <Image source={require('../../assets/NavBarIcons/StatsIcon.png')} />
+      <Image source={require('../../assets/NavBarIcons/SelectedStats1.png')} style={styles.icon}/> :
+      <Image source={require('../../assets/NavBarIcons/StatsIcon.png')} style={styles.icon}/>
   )
 }
 
@@ -43,8 +44,6 @@ const navBarScreenOptions = {
   tabBarHideOnKeyboard: true,
   tabBarStyle: {
     backgroundColor: '#5BC0BE',
-    height: 90
-    
   }
 }
 
@@ -58,3 +57,9 @@ export default function Navbar(): JSX.Element {
     </Tab.Navigator>
   )
 }
+const styles = StyleSheet.create({
+  icon:{
+    height: height < 900 ? (48 *.85):48,
+    width: width < 420 ? (48 * .85):48
+  }
+})
