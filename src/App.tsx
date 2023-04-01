d902ac32a44406965783d8e738cf57afa865995a
Conroy Ricketts <conroyr41@gmail.com>
2023-04-01 12:21:25 -0400

    Trying to fix folder name.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { NavigationContainer } from '@react-navigation/native'
 import { registerRootComponent } from 'expo'
 import { StatusBar } from 'expo-status-bar'
 import React from 'react'
-import Navbar from './Components/Navbar'
+import Navbar from './components/Navbar'
 
 export default function App(): JSX.Element {
     return (
