5679d7c525b1adfe78ecb6647e8334f3c77b1aa6
Jordan Miller <Jordan.miller7110@gmail.com>
2023-04-16 17:00:09 -0400

    Fixes that hopefully fix the problem

--- src/helpers/WorkoutPlanDataHelper.ts
+++ src/helpers/WorkoutPlanDataHelper.ts
@@ -21,6 +21,7 @@ export interface ExerciseModel {
   //   sets: SetModel[];
   // }
 
+
 export function workoutSetUsesReps(
     workoutSet: WorkoutSetWithReps | WorkoutSetWithTime
 ): workoutSet is WorkoutSetWithReps {
@@ -34,53 +35,63 @@ export function convertSecondsToTimeFormat(seconds: number): string {
 }
 
 export const getWorkoutPlan = async (exp_level: number): Promise<WorkoutModel[]> => {
-  const exercises = await fetchExercises();
-
-  // Get only exercises that are resistance or cardio
-  const filteredExercises = exercises.filter((exercise: any) => {
-    return exercise.category === 9 || exercise.category === 10;
-  });
-
-  // Take the first 10 exercises
-  const selectedExercises = filteredExercises.slice(0, 10);
-
-  // Map the selected exercises to WorkoutModels
-  const workoutPlan: WorkoutModel[] = selectedExercises.map((exercise: any) => {
-    let set: SetModel;
-    if (exercise.category === 9) {
-      // Resistance exercise
-      switch (exp_level) {
-        case 1:
-          set = { weight: 10, reps: 12 };
-          break;
-        case 2:
-          set = { weight: 20, reps: 12 };
-          break;
-        case 3:
-          set = { weight: 25, reps: 12 };
-          break;
-        default:
-          set = { weight: 10, reps: 12 };
-      }
-    } else {
-      // Cardio exercise
-      switch (exp_level) {
-        case 1:
-          set = { seconds: 30 };
-          break;
-        case 2:
-          set = { seconds: 60 };
-          break;
-        case 3:
-          set = { seconds: 90 };
-          break;
-        default:
-          set = { seconds: 30 };
-      }
+  try {
+    const exercisesResponse = await fetchExercises();
+    if (!Array.isArray(exercisesResponse.results)) {
+      console.error('fetchExercises did not return an array:', exercisesResponse.results);
+      return [];
     }
-    return { name: exercise.name, sets: [set] };
-  });
 
-  return workoutPlan;
+    // Get only exercises that are resistance or cardio
+    const filteredExercises = exercisesResponse.results.filter((exercise: any) => {
+      return exercise.category === 9 || exercise.category === 10;
+    });
+
+    // Take the first 10 exercises
+    const selectedExercises = filteredExercises.slice(0, 10);
+
+    // Map the selected exercises to WorkoutModels
+    const workoutPlan: WorkoutModel[] = selectedExercises.map((exercise: any) => {
+      let set: WorkoutSetWithReps | WorkoutSetWithTime;
+
+      if (exercise.category === 9) {
+        // Resistance exercise
+        switch (exp_level) {
+          case 1:
+            set = { weight: 10, reps: 12 };
+            break;
+          case 2:
+            set = { weight: 20, reps: 12 };
+            break;
+          case 3:
+            set = { weight: 25, reps: 12 };
+            break;
+          default:
+            set = { weight: 10, reps: 12 };
+        }
+      } else {
+        // Cardio exercise
+        switch (exp_level) {
+          case 1:
+            set = { seconds: 30 };
+            break;
+          case 2:
+            set = { seconds: 60 };
+            break;
+          case 3:
+            set = { seconds: 90 };
+            break;
+          default:
+            set = { seconds: 30 };
+        }
+      }
+      return { name: exercise.name, sets: [set] };
+    });
+
+    return workoutPlan;
+  } catch (error) {
+    console.error('Failed to fetch exercises', error);
+    return [];
+  }
 };
   
--- src/models/WorkoutModel.ts
+++ src/models/WorkoutModel.ts
@@ -7,7 +7,6 @@ interface WorkoutSetWithReps {
     weight: number,
     reps: number,
 }
-
 interface WorkoutSetWithTime {
     seconds: number
 }
